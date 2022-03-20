579d545427947f3c4d7afa92270bc6bed9712284
israellinofaustino <israellinofaustino@hotmail.com>
2022-03-20 18:38:13 +0000

    new_db_access

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 require_once("config.php");
 date_default_timezone_set('Europe/London');
-$db = parse_url(getenv("postgres://exnsqayubcquva:18c853276acedc59be44deea3bbf24ddece34e943f862e68d188c84f87e18b1e@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d746d39isninal"));
+$db = parse_url(getenv("postgres://rmlnlaizxnvoly:36d89b7cb1739a10c4f4545ec22d883818ba48cca9d04935e2864626e9484354@ec2-54-247-137-184.eu-west-1.compute.amazonaws.com:5432/d641e6ul1sj6e2"));
 try{
-    $pdo = new PDO("pgsql:" . sprintf("host=ec2-176-34-105-15.eu-west-1.compute.amazonaws.com;port=5432;user=exnsqayubcquva;password=18c853276acedc59be44deea3bbf24ddece34e943f862e68d188c84f87e18b1e;dbname=d746d39isninal",
+    $pdo = new PDO("pgsql:" . sprintf("host=ec2-54-247-137-184.eu-west-1.compute.amazonaws.com;port=5432;user=rmlnlaizxnvoly;password=36d89b7cb1739a10c4f4545ec22d883818ba48cca9d04935e2864626e9484354;dbname=d641e6ul1sj6e2",
         ltrim($db["path"], "/")
     ));    
 }catch(Exception $e){
